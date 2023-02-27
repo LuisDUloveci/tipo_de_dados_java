a25ce0a7b7c558bae09aa507b844467de5574db4
LuisDUloveci <luiskrauzinho@gmail.com>
2023-02-27 15:17:30 +0000

    salvar

---
+++ EstruturaDeSelecao.java
@@ -0,0 +1,18 @@
+
+public class EstruturaDeSelecao {
+    public static void main(String[] args) {
+        int idade;
+
+        idade=19;
+
+        if (idade >= 18)
+        {
+            System.out.print("apto ao serviço Militar Obrigatório!");
+
+
+        }else{
+            System.out.print("Não esta apto ao Serviço Militar Obrigatório!");
+        }
+    }
+    
+}
